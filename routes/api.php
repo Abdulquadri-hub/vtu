bfc0e1bd739825fe4910557b4141c71249bddfcc
Abdul Quadri <abdulquadri.aq@gmail.com>
2025-01-26 17:11:56 +0100

    setting up api server

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/', function() {
+    return response()->json([
+        "status" => "The api server is ready and working!"
+    ]);
+});
