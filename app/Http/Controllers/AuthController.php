840cfb8926c37aa2ac3e23c3c5ef8712bb5b3004
Abdul Quadri <abdulquadri.aq@gmail.com>
2025-01-26 17:13:32 +0100

    setting up api server

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Traits\ApiResponseHandler;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    use ApiResponseHandler;
 }
